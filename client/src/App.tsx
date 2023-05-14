f99867a8bc087f7122c9e1c6a4532333134f79cb
kenneth <itskcheng@gmail.com>
2023-05-14 16:25:29 -0400

    Setting up client folder

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-600 text-center">
+        Money Tracker
+      </h1>
+    </>
+  );
+}
+
+export default App;
